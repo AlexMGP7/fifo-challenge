3733bf2e099159a654feeeadc632657ac5d53a3d
Carlos Jimenez <74768457+Carlos-jim@users.noreply.github.com>
2025-02-21 19:58:05 -0400

    Deleted file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { FC } from "react";
-import ThemeContainer from "./components/themeContainer";
 import AppRouter from "./router/appRouter";
 import { CartProvider } from "./context/cartContext";
 
 const App: FC = () => {
   return (
     <CartProvider>
-      <ThemeContainer>
-        <AppRouter />
-      </ThemeContainer>
+      <AppRouter />
     </CartProvider>
   );
 };
--- src/router/appRouter.tsx
+++ src/router/appRouter.tsx
@@ -6,15 +6,10 @@ import {
   Navigate,
 } from "react-router-dom";
 import ProtectedLayout from "../layouts/protectedLayout";
-import Tutorial from "../components/tutorial";
 import Dashboard from "../pages/dashboard";
-import Login from "../pages/login";
-import AddProduct from "../pages/addProduct";
-import History from "../pages/history";
 import CatalogMenFranelasPage from "../pages/hombres-franelas";
 import CatalogMenZapatosPage from "../pages/hombres-zapatos";
 import CatalogMenPantalonesPage from "../pages/hombres-pantalones";
-import { CartProvider } from "../context/cartContext"; // Importar el CartProvider
 
 const AppRouter: FC = () => {
   // Si ya no necesitas el contexto de autenticación, puedes eliminar esta línea.
@@ -29,13 +24,11 @@ const AppRouter: FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/login" element={<Login />} />
         <Route
           path="/dashboard"
           element={
             <ProtectedLayout>
               <>
-                <Tutorial />
                 <Dashboard />
               </>
             </ProtectedLayout>
@@ -46,7 +39,6 @@ const AppRouter: FC = () => {
           element={
             <ProtectedLayout>
               <>
-                <Tutorial />
                 <CatalogMenFranelasPage />
               </>
             </ProtectedLayout>
@@ -57,7 +49,6 @@ const AppRouter: FC = () => {
           element={
             <ProtectedLayout>
               <>
-                <Tutorial />
                 <CatalogMenZapatosPage />
               </>
             </ProtectedLayout>
@@ -68,30 +59,7 @@ const AppRouter: FC = () => {
           element={
             <ProtectedLayout>
               <>
-                  <Tutorial />
                   <CatalogMenPantalonesPage />
-              </>
-            </ProtectedLayout>
-          }
-        />
-        <Route
-          path="/add-product"
-          element={
-            <ProtectedLayout>
-              <>
-                <Tutorial />
-                <AddProduct />
-              </>
-            </ProtectedLayout>
-          }
-        />
-        <Route
-          path="/history"
-          element={
-            <ProtectedLayout>
-              <>
-                <Tutorial />
-                <History />
               </>
             </ProtectedLayout>
           }
